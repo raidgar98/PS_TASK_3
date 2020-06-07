3137ff64456fe26edd16a0f088109f3a57e494cb
raidgar98 <raidgar98@onet.pl>
2020-06-07 02:21:11 +0200

    works

---
+++ libraries/graphic/components/label/label.cpp
@@ -0,0 +1,36 @@
+#include "label.h"
+#include <GL/glut.h>
+
+void Label::render(drawing_instruction_collection &collection)
+{
+	constexpr number FONT_HEIGHT{12.0};
+	update_required = false;
+	const Dimension& pad = padding(); // aliasing
+
+	// Calculate minimal size of label
+	Dimension minimal_caption_size( pad.width * 2.0, FONT_HEIGHT + (pad.height * 2.0), SCREEN );
+	for (const str::value_type c : caption)
+		minimal_caption_size.width = minimal_caption_size.width + glutBitmapWidth(GLUT_BITMAP_HELVETICA_12, c);
+	
+	minimal_caption_size = minimal_caption_size.to_cartesian();
+	actual_padding = padding().to_cartesian();
+
+	// Adjust width
+	if( minimal_caption_size.width >= orginal_dimension.width ) dimensions.width = minimal_caption_size.width;
+	else actual_padding.width = actual_padding.width + ( (orginal_dimension.width - minimal_caption_size.width ) / 2.0 );
+
+	// Adjust height
+	if( minimal_caption_size.height >= orginal_dimension.height ) dimensions.height = minimal_caption_size.height;
+	else actual_padding.height = actual_padding.height + ( (orginal_dimension.height - minimal_caption_size.height ) / 2.0 );
+
+	RectangleComponent::render(collection);
+}
+
+void Label::additional_render_instruction() const
+{
+	const Dimension& pad = actual_padding; // aliasing
+	glColor4d(text_color().r, text_color().g, text_color().b, text_color().a);
+	glRasterPos2d(point.x + pad.width, point.y - dimensions.height + ( pad.height ) );
+	for (const str::value_type c : caption)
+		glutBitmapCharacter(GLUT_BITMAP_HELVETICA_12, c);
+}
---
+++ libraries/graphic/components/label/label.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <functional>
+
+#include "../frame/frame.h"
+#include "../rectangle_component/rectangle_component.h"
+
+struct Label : RectangleComponent
+{
+  Property<Color> text_color;
+  Property<Dimension> padding;
+
+  Label(const Point &pnt, const Dimension &dmn, const str &caption, const Color &text_color = Colors::black, const Color &background_color = Colors::invisible, const Dimension &_padding = Dimension{5.0, 5.0, SCREEN}.to_cartesian())
+      : RectangleComponent{pnt, dmn, background_color}, caption{caption}, text_color{text_color}
+  {
+    if (_padding.system != SCREEN) padding = _padding.to_pixel();
+    else padding = _padding;
+
+    if(dmn.system != CARTESIAN) orginal_dimension = dmn.to_cartesian();
+    else orginal_dimension = dmn;
+  }
+
+  virtual void render(drawing_instruction_collection &) override;
+  virtual bool move() override { return update_required; };
+  void Caption(const str &text)
+  {
+    caption = text;
+    update_required = true;
+  }
+  virtual void additional_render_instruction() const override;
+  const str &Caption() const { return caption; }
+  virtual void resize() override { update_required = true; }
+
+protected:
+  std::string caption;
+  bool update_required = false;
+  Dimension actual_padding;
+  Dimension orginal_dimension;
+};
